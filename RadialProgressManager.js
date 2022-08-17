64ab71bb64fc8fa7e6675d00c40b524d4d86f343
ponyosushi <ponyosushi@gmail.com>
2022-08-17 14:19:03 +0900

    모달창 모듈화

---
+++ RadialProgressManager.js
@@ -0,0 +1,111 @@
+import {initResultModalStyleTemp, initResultModalTemp} from './js/utils/templates.js'                        // 샘플데이터를 세팅모듈을 가져온다.
+
+/**
+ * 원형차트 매니저
+ * @param {원 사이즈} size 
+ * @param {className} id 
+ * @param {바 사이즈} barSize 
+ * @param {바 색깔} barColor 
+ * @param {백그라운드 색깔} backgroundColor 
+ * @param {순서} zIndex 
+ */
+function RadialProgressManager(parentId, id, size,  barSize, barColor, backgroundColor, zIndex)  {
+    let progress = 0;
+    let requestAnimationFrame = window.requestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame;
+
+    // 선언과 동시에 템플릿을 생성시킨다.
+    initResultModalStyleTemp(id, size, barSize, barColor, backgroundColor, zIndex);
+    initResultModalTemp(parentId, id);
+
+    // 원형차트 삭제
+    const remove = () => {
+        let radialProgress = document.getElementsByClassName("radial-progress" + id)[0];
+        let style = document.getElementById(id + "Css");
+        let scale = 1;
+        let deltaScale = 0.1 / 10;
+
+        function step() {
+            scale += deltaScale;
+            scale = (scale < 0) ? 0 : scale;
+            radialProgress.style.transform = 'scale(' + scale + ')';
+
+            if (scale > 1.1) {
+                deltaScale = -1.1 / 8;
+            }
+
+            if (scale > 0) {
+                requestAnimationFrame(step);
+            } else {
+                document.getElementsByTagName('head')[0].removeChild(style);
+                document.getElementById("resultModal").removeChild(radialProgress);
+            }
+        }
+
+        requestAnimationFrame(step);
+    };
+
+    // 퍼센트바 세팅
+    const setProgress = (percent, duration, id) => {
+        let radialProgress = document.getElementsByClassName("radial-progress" + id)[0];
+        percent = (percent > 100) ? 100 : percent;
+        let $maskFull = radialProgress.getElementsByClassName('mask' + id + ' full' + id)[0];
+        let $fill = radialProgress.getElementsByClassName('fill' + id);
+        let $fillFix = radialProgress.getElementsByClassName('fill' + id + ' fix' + id)[0];
+        let deltaProgress = (percent - progress) / (duration * 60);
+
+        function step() {
+            progress += deltaProgress;
+            progress = (progress > percent) ? percent : progress;
+            let rotate = progress * 1.8;
+            $maskFull.style.transform = 'rotate(' + rotate + 'deg)';
+
+            for (var i = 0; i < $fill.length; ++i) {
+                $fill[i].style.transform = 'rotate(' + rotate + 'deg)';
+            }
+
+            $fillFix.style.transform = 'rotate(' + 2 * rotate + 'deg)';
+
+            if (progress < percent) {
+                requestAnimationFrame(step);
+            }
+
+            // if (progress === 100) {
+            //     setTimeout(function() {
+                    // remove();   // 종료
+            //     }, 1000);
+            // }
+        }
+
+        requestAnimationFrame(step);
+    };  
+
+    return {
+        remove,
+        setProgress
+    }
+};
+
+
+const createProgress = () => {
+    document.getElementById("confirm").addEventListener('click', function () {
+        document.getElementsByClassName("modalWrap")[0].classList.remove("none");
+
+
+        // 사이즈, id, 바 사이즈, 바 색깔, 백그라운드 색깔, z-index
+        var progress = new RadialProgressManager("circleChart", 'A', 180, 10, '#EB6540', '#FFF', '60');
+        var progress2 = new RadialProgressManager("circleChart", 'B', 130, 10, '#48A7BD', '#FFF', '65');
+        var progress3 = new RadialProgressManager("circleChart", 'C', 80, 10, '#A66EF5', '#FFF', '70');
+
+        setTimeout(function() {
+            progress.setProgress(60, 0.5, "A");
+            progress2.setProgress(80, 0.5, "B");
+            progress3.setProgress(90, 0.5, "C");
+        }, 1000);
+    });
+}
+
+createProgress();
--- js/utils/templates.js
+++ js/utils/templates.js
@@ -3,7 +3,7 @@
  * @param {TypingMockData.Content} data 
  * @returns 
  */
-export const createTypingTemp = data => {
+const createTypingTemp = data => {
     return `<span class="normal" id="tempContents">${data}</span>`
 }
 
@@ -24,7 +24,7 @@ export const initTypingTemp = (data) => {
  * @param {레벨} level 
  * @returns 
  */
-export const createNormalRoom = level => {
+const createNormalRoom = level => {
     return `<object class="room" data="./img/normal/level${level}.svg" type="image/svg+xml" id="room"></object>`
 }
 
@@ -42,3 +42,56 @@ export const createNormalRoom = level => {
             break;
     }
 }
+
+/**
+ * 결과창모달 스타일 템플릿
+ * @param {원의 사이즈} size 
+ * @param {className} id 
+ * @param {바 사이즈} barSize 
+ * @param {바 색깔} barColor 
+ * @param {백그라운드 색깔} backgroundColor 
+ * @param {순서} zIndex 
+ * @param {innerSize} innerSize 
+ * @param {innerMargin} innerMargin 
+ */
+const createResultModalStyle = (id, size, barSize, barColor, backgroundColor, zIndex, innerSize, innerMargin) => {
+    return `<style type="text/css" id="${id}Css">.radial-progress${id} {width: ${size}px;height: ${size}px;position: absolute;margin: auto;top: 0; right: 0; bottom: 0; left: 0;z-index: ${zIndex};background-color: #DDD;border-radius: 50%;box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.2);}.radial-progress${id} .inner-circle${id} {width: ${innerSize}px;height: ${innerSize}px;position: absolute;margin-top: ${innerMargin}px;margin-left: ${innerMargin}px;background-color: ${backgroundColor};border-radius: 50%;z-index: 10;}.radial-progress${id} .outer-circle${id} .mask${id},.radial-progress${id} .outer-circle${id} .fill${id} {width: ${size}px;height: ${size}px;position: absolute;border-radius: 50%;-webkit-backface-visibility: hidden;}.radial-progress${id} .outer-circle${id} .mask${id} {clip: rect(0px, ${size}px, ${size}px, ${size / 2}px);}.radial-progress${id} .outer-circle${id} .mask${id} .fill${id} {clip: rect(0px, ${size / 2}px, ${size}px, 0px);background-color: ${barColor};}</style>`
+}
+
+/**
+ * 결과창모달 스타일을 세팅한다.
+ * @param {className} id 
+ * @param {원의 사이즈} size 
+ * @param {바 사이즈} barSize 
+ * @param {바 색깔} barColor 
+ * @param {백그라운드 색깔} backgroundColor 
+ * @param {순서} zIndex 
+ */
+export const initResultModalStyleTemp = (id, size, barSize, barColor, backgroundColor, zIndex) => {
+    barSize = (barSize % 2 === 1) ? (barSize + 1) : barSize;
+    let innerSize = size - barSize;
+    let innerMargin = barSize / 2;
+    document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', createResultModalStyle(id, size, barSize, barColor, backgroundColor, zIndex, innerSize, innerMargin));
+}
+
+/**
+ * 결과창모달 템플릿
+ * @param {className} id 
+ */
+export const createResultModal = (id) => {
+    return `<div class="radial-progress${id}"><div class="inner-circle${id}"></div><div class="outer-circle${id}"><div class="mask${id} full${id}"><div class="fill${id}"></div></div><div class="mask${id}"><div class="fill${id}"></div><div class="fill${id} fix${id}"></div></div></div></div>`
+}
+
+/**
+ * 결과창모달을 세팅한다.
+ * @param {부모의 id} parentId 
+ * @param {className} id 
+ */
+export const initResultModalTemp = (parentId, id) => {
+    document.getElementById(parentId).insertAdjacentHTML('beforeend', createResultModal(id));
+}
+
+
+
+
+
